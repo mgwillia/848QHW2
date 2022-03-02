8727c235affcedce5bc64ce3d9325f8b747c763b
Matt Gwilliam <mattgwilliamjr@gmail.com>
2022-03-02 14:55:31 -0500

    y

---
+++ batch_train_and_eval.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=matt_job
+#SBATCH --output=logs/matt_job.out.%j
+#SBATCH --error=logs/matt_job.out.%j
+#SBATCH --time=36:00:00
+#SBATCH --account=abhinav
+#SBATCH --partition=scavenger
+#SBATCH --qos=scavenger
+#SBATCH --gres=gpu:gtx1080ti:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+
+module load cuda/11.0.3
+
+srun bash -c "hostname;"
+srun bash -c "tfidf_guesser.py \
+    --guesstrain data/qanta.train.2018.json \
+    --guessdev data/qanta.dev.2018.json \
+    --model_path models/tfidf.pickle;"
+
+srun bash -c "python run_e2e_eval.py;"
