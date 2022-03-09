6141a742be9abaff21c794b536e175713c44fcfa
Matt Gwilliam <mattgwilliamjr@gmail.com>
2022-03-09 17:30:34 -0500

    updates

--- guess_train_dataset.py
+++ guess_train_dataset.py
@@ -25,33 +25,24 @@ class GuessTrainDataset(torch.utils.data.Dataset):
             with open(f"data/{dataset_name}_answers.pkl", "rb") as fp:
                 self.answers = pickle.load(fp)
         else:
-
             self.questions = []
             self.answer_pages = []
             self.answers = []
-            answers_buffer = []
             print(f'Preparing dataset with {len(questions)} entries')
             for i, (question, page) in enumerate(zip(questions, answers)):
-                answers_buffer.append(wiki_lookup[page]['text'])
-
-                if len(answers_buffer) == 100:
-                    answer_toks_batch = tokenizer(answers_buffer, return_tensors="pt", max_length=512, truncation=True, padding=True)["input_ids"]
-                    self.answers.extend(answer_toks_batch)
-                    answers_buffer = []
-
-                
-                question_toks = tokenizer(question, return_tensors="pt", max_length=512, truncation=True, padding=True)["input_ids"]
-                #answer_tok = tokenizer(wiki_lookup[page]['text'], return_tensors="pt", max_length=512, truncation=True, padding=True)["input_ids"]
+                question_toks = []
+                for question_sentence in question:
+                    question_toks.append(tokenizer(question_sentence, return_tensors="pt", max_length=512, truncation=True, padding='max_length')["input_ids"])
+                answer_tok = tokenizer(wiki_lookup[page]['text'], return_tensors="pt", max_length=512, truncation=True, padding='max_length')["input_ids"]
+                #print(question_toks[0].shape)
+                #print(answer_tok.shape)
 
                 self.questions.append(question_toks)
                 self.answer_pages.append(page)
-                #self.answers.append(answer_tok)
+                self.answers.append(answer_tok)
 
                 if i % 1000 == 0:
                     print(f'Tokenized {i} questions and answers', flush=True)
-            if len(answers_buffer) > 0:
-                answer_toks_batch = tokenizer(answers_buffer, return_tensors="pt", max_length=512, truncation=True, padding=True)["input_ids"]
-                self.answers.extend(answer_toks_batch)
 
             with open(f"data/{dataset_name}_questions.pkl", "wb") as fp:
                 pickle.dump(self.questions, fp)        
@@ -64,7 +55,9 @@ class GuessTrainDataset(torch.utils.data.Dataset):
         return len(self.questions)
 
     def __getitem__(self, index):
-        
-        out = {'question': self.questions[index][random.randint(0, len(self.questions[index]))], 'answer_text': self.answers[index], 'answer_page': self.answer_pages[index]}
+        for q in self.questions[index]:
+            print(q.shape)
+        print(self.answers[index].shape)
+        out = {'question': self.questions[index][random.randint(0, len(self.questions[index])-1)], 'answer_text': self.answers[index], 'answer_page': self.answer_pages[index]}
 
         return out
